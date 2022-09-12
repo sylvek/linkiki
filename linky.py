ce004d8b18666cc4d6fb2011c58dc53dc7688ee0
Sylvain Maucourt <smaucourt@gmail.com>
2022-09-12 19:29:12 +0200

    instead of publishing PAPP which is not what is billed by EDF and which is less accurate and in "VA". I decided to calculate the actual current power by using HCHC and HCHP consumption which is in "wh" by transforming them in W

--- linky.py
+++ linky.py
@@ -5,11 +5,11 @@ class Linky:
         # Lecture d'eventuel caractere avant le debut de trame
         # Jusqu'au caractere \x02 + \n (= \x0a)
         _trame = list()
-        while _trame[-2:]!=['\x02','\n']:
+        while _trame[-2:] != ['\x02', '\n']:
             _trame.append(ser.read(1))
         # Lecture de la trame jusqu'au caractere \x03
-        _trame=list()
-        while _trame[-1:]!=['\x03']:
+        _trame = list()
+        while _trame[-1:] != ['\x03']:
             _trame.append(ser.read(1))
         # Suppression des caracteres de fin '\x03' et '\r' de la liste
         _trame.pop()
@@ -22,14 +22,14 @@ class Linky:
         result = {}
         for ligne in lignes:
             _tuple = self._valid(ligne)
-            result[_tuple[0]]=_tuple[1]
+            result[_tuple[0]] = _tuple[1]
         return result
 
     def _valid(self, ligne):
         """Retourne les elements d'une ligne sous forme de tuple si le checksum est ok"""
         chk = self._checksum(ligne)
         items = ligne.split(' ')
-        if ligne[-1]==chk:
+        if ligne[-1] == chk:
             return (items[0], items[1])
         else:
             raise IOError("Checksum error")
@@ -38,6 +38,6 @@ class Linky:
         """Verifie le checksum d'une ligne et retourne un tuple"""
         _sum = 0
         for ch in ligne[:-2]:
-                _sum += ord(ch)
+            _sum += ord(ch)
         _sum = (_sum & 63) + 32
         return chr(_sum)
--- main.py
+++ main.py
@@ -12,14 +12,20 @@ import linky
 
 linky = linky.Linky()
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',filename='linkiki.log', filemode='w', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='linkiki.log', filemode='w', level=logging.INFO)
 logger = logging.getLogger("linkiki")
 
-parser = argparse.ArgumentParser(description='fetch data from linky and push it to mqtt')
-parser.add_argument('usbport', metavar='usbport', help='usb port like /dev/serial0', nargs='?', default='/dev/serial0')
-parser.add_argument('hostname', metavar='hostname', help='hostname of mqtt server', nargs='?', default="0.0.0.0")
-parser.add_argument('port', metavar='port', help='port of mqtt server', nargs='?', default="1883")
-parser.add_argument('topic', metavar='topic', help='topic used to publish data', nargs='?', default="sensors/linky/watt")
+parser = argparse.ArgumentParser(
+    description='fetch data from linky and push it to mqtt')
+parser.add_argument('usbport', metavar='usbport',
+                    help='usb port like /dev/serial0', nargs='?', default='/dev/serial0')
+parser.add_argument('hostname', metavar='hostname',
+                    help='hostname of mqtt server', nargs='?', default="0.0.0.0")
+parser.add_argument('port', metavar='port',
+                    help='port of mqtt server', nargs='?', default="1883")
+parser.add_argument('topic', metavar='topic', help='topic used to publish data',
+                    nargs='?', default="sensors/linky/watt")
 args = parser.parse_args()
 
 usbport = args.usbport
@@ -27,18 +33,22 @@ run = True
 previousValue = -1
 previousTimestamp = -1
 
+
 def signal_handler(signal, frame):
     global run
     logger.info("Ending and cleaning up")
     run = False
 
+
 def handle_debug(signal, frame):
     logger.info("Switching to DEBUG level")
     logger.setLevel(logging.DEBUG)
 
+
 def handle_info(signal, frame):
     logger.info("Switching to INFO level")
     logger.setLevel(logging.INFO)
+
 
 try:
     logger.info("Starting listening Linky")
@@ -53,7 +63,8 @@ try:
         logger.debug("waiting for %s", usbport)
         time.sleep(2)
 
-    ser = serial.Serial(port=usbport, baudrate=1200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=1)
+    ser = serial.Serial(port=usbport, baudrate=1200, bytesize=serial.SEVENBITS,
+                        parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE, timeout=1)
 
     client = mqtt.Client()
     client.connect(args.hostname, int(args.port), 60)
@@ -67,13 +78,16 @@ except Exception as e:
 while run:
     try:
         lignes = linky.read(ser)
-        newValue = lignes["PAPP"]
+        newValue = int(lignes["HCHC"])+int(lignes["HCHP"])
         newTimestamp = time.time()
-        if newValue != previousValue or newTimestamp > (previousTimestamp + 10):
+        if newValue != previousValue:
             logger.debug("new value: %s", newValue)
+            if previousValue != -1:
+                currentConsumption = int(
+                    round((newValue - previousValue)/(newTimestamp - previousTimestamp)*3600))
+                client.publish(args.topic, currentConsumption)
             previousValue = newValue
             previousTimestamp = newTimestamp
-            client.publish(args.topic, newValue)
     except Exception as e:
         logger.exception("error in main loop. %s", e)
 
